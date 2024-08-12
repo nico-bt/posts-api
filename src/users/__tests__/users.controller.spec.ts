b812fd5c3f1e1d4cf9847cdb8d020c374f640614
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-08-11 21:23:41 -0300

    Add unit tests for users

--- src/users/__tests__/users.controller.spec.ts
+++ src/users/__tests__/users.controller.spec.ts
@@ -1,20 +1,131 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UsersController } from '../users.controller';
+import { UsersService } from '../users.service';
+import { AuthService } from '../auth.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { SigninUserDto } from '../dto/signin-user.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { NotFoundException } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+
+const mockUsersService = {
+  findAll: jest.fn(),
+  findOneById: jest.fn(),
+};
+
+const mockAuthService = {
+  signup: jest.fn(),
+  signin: jest.fn(),
+};
+
+const mockAuthGuard = {
+  canActivate: jest.fn().mockReturnValue(true),
+};
+
+const mockUser = {
+  id: 1,
+  email: 'test.user@test.com',
+  firstname: 'Testing',
+  lastname: 'User',
+} as User;
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(mockAuthGuard)
+      .compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  describe('create (signup)', () => {
+    it('should create a new user', async () => {
+      const createUserDto: CreateUserDto = {
+        email: mockUser.email,
+        firstname: mockUser.firstname,
+        lastname: mockUser.lastname,
+        password: 'testpassword',
+      };
+
+      const token = { token: 'jwt.token' };
+      jest.spyOn(authService, 'signup').mockResolvedValue(token);
+
+      const result = await usersController.create(createUserDto);
+
+      expect(result).toEqual(token);
+      expect(authService.signup).toHaveBeenCalledWith(createUserDto);
+    });
+  });
+
+  describe('signin', () => {
+    it('should sign in a user', async () => {
+      const signinUserDto: SigninUserDto = {
+        email: 'test.user@test.com',
+        password: 'testpassword',
+      };
+
+      const mockSigninResponse = { token: 'jwt.token' };
+      jest.spyOn(authService, 'signin').mockResolvedValue(mockSigninResponse);
+
+      const result = await usersController.signin(signinUserDto);
+
+      expect(result).toEqual(mockSigninResponse);
+      expect(authService.signin).toHaveBeenCalledWith(signinUserDto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const mockUsersArray = [mockUser];
+      jest.spyOn(usersService, 'findAll').mockResolvedValue(mockUsersArray);
+
+      const result = await usersController.findAll();
+
+      expect(result).toEqual(mockUsersArray);
+      expect(usersService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single user by id', async () => {
+      const ID = mockUser.id;
+      jest.spyOn(usersService, 'findOneById').mockResolvedValue(mockUser);
+
+      const result = await usersController.findOne(ID);
+
+      expect(result).toEqual(mockUser);
+      expect(usersService.findOneById).toHaveBeenCalledWith(ID);
+    });
+
+    it('should throw NotFoundException if user not found', async () => {
+      jest.spyOn(usersService, 'findOneById').mockResolvedValue(null);
+
+      await expect(usersController.findOne(2)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
   });
 });
--- src/users/__tests__/users.service.spec.ts
+++ src/users/__tests__/users.service.spec.ts
@@ -1,18 +1,124 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { UsersService } from '../users.service';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateUserDto } from '../dto/create-user.dto';
+
+const mockUser: User = {
+  id: 1,
+  email: 'test.user@test.com',
+  firstname: 'Testing',
+  lastname: 'User',
+  password: 'testpassword',
+  posts: [],
+};
+
+const mockUserRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create and save a new user', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'test.user@test.com',
+        firstname: mockUser.firstname,
+        lastname: mockUser.lastname,
+        password: 'testpassword',
+      };
+
+      mockUserRepository.create.mockReturnValue(mockUser);
+      mockUserRepository.save.mockResolvedValue(mockUser);
+
+      const result = await service.create(createUserDto);
+
+      expect(repository.create).toHaveBeenCalledWith(createUserDto);
+      expect(repository.save).toHaveBeenCalledWith(mockUser);
+      expect(result).toEqual(mockUser);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const usersArray = [mockUser];
+      mockUserRepository.find.mockResolvedValue(usersArray);
+
+      const result = await service.findAll();
+
+      expect(repository.find).toHaveBeenCalled();
+      expect(result).toEqual(usersArray);
+    });
+  });
+
+  describe('findOneById', () => {
+    it('should return a single user by id', async () => {
+      mockUserRepository.findOne.mockResolvedValue(mockUser);
+
+      const result = await service.findOneById(mockUser.id);
+
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: { id: mockUser.id },
+      });
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return null if user is not found', async () => {
+      mockUserRepository.findOne.mockResolvedValue(null);
+
+      const result = await service.findOneById(2);
+
+      expect(repository.findOne).toHaveBeenCalledWith({ where: { id: 2 } });
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('findOneByEmail', () => {
+    it('should return a single user by email', async () => {
+      mockUserRepository.findOne.mockResolvedValue(mockUser);
+
+      const result = await service.findOneByEmail(mockUser.email);
+
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: { email: mockUser.email },
+      });
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return null if user is not found', async () => {
+      mockUserRepository.findOne.mockResolvedValue(null);
+
+      const result = await service.findOneByEmail('nonexistent@test.com');
+
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: { email: 'nonexistent@test.com' },
+      });
+      expect(result).toBeNull();
+    });
   });
 });
